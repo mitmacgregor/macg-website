472bff1d51c1875fb70c73483a7501b14573bbe4
OrionLi545 <haoxuanl@mit.edu>
2022-10-14 11:22:58 -0400

    Initial Infrastructure

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// Import our custom CSS
+import '../scss/common.scss'
+
+// Import the CSS for the page
+import '../scss/index.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+    about: './src/js/about.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    assetModuleFilename: 'images/[name][ext]'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
