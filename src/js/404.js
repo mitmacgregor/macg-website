c798a5984f5b9381460787b2c5fb826741f99d45
hakan <hakan.chunton@gmail.com>
2024-04-19 05:59:33 -0400

    Trial

--- src/js/404.js
+++
@@ -1,5 +0,0 @@
-// Import our custom CSS
-import '../scss/common.scss'
-
-// Import all of Bootstrap's JS
-import Dropdown from 'bootstrap/js/dist/dropdown.js'
--- src/js/reservation.js
+++
@@ -1,8 +0,0 @@
-// Import our custom CSS
-import '../scss/common.scss'
-
-// Import the CSS for the page
-import '../scss/reservation.scss'
-
-// Import all of Bootstrap's JS
-import Dropdown from 'bootstrap/js/dist/dropdown.js'
